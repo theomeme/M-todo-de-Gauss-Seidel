7dd8a5805c1c2c53e16c38ef4f9f37e2e4624a96
Theodoro <theomeme21@gmail.com>
2022-05-10 12:56:24 -0300

    correcoes e alteracoes

--- src/source.py
+++ src/source.py
@@ -1,6 +1,5 @@
 def haZeroNaDiagonal(m):
-    #verificação de há zero na diagona principal
-    #retorna a quantidade de zeros na diagonal principal
+    
     qtdDeZeros = 0
     posicao = 0
     while posicao < len(m):
@@ -10,24 +9,20 @@ def haZeroNaDiagonal(m):
 
 
 def poeUmNaDiagonalPrincipalNaLinha(lin, m):
-    #Adiciona o número 1 na diagonal principal
-    #Parametros = {number}Linha / {variavel}Matriz
-    #Retorna a a linha com o 1 na diagonal principal
     divisor = m[lin][lin]
     col = 0
     while col <= len(m):
         m[lin][col] /= divisor
         col += 1
 
-def seNaoETudoZero(m, col):
-    #Verifica se há um zero na matriz , varrendo a matriz inteira por linha e coluna
-    #Parametro = {variavel} Matriz
-    # Retorna {boolen} True / False
-    vetor_negativo = []
-    for lin in range(len(m) - 1):
-        if lin != col:
-            if m[lin][col] != 0:
-                vetor_negativo[lin][col] = m[lin][col] * (-1)
+def seNaoETudoZero(m):
+
+    for col in range(len(m)):
+        for lin in range(len(m)):
+            if lin != col:
+                if m[lin][col] != 0:
+                    m[lin][col]
+                    vetor_negativo = m[lin]
 def ondeTemZero():
     while haZeroNaDiagonal(matriz):
         count = 0
@@ -52,10 +47,8 @@ def __main__(matriz):
     if not haZeroNaDiagonal(matriz):
         for i in range(length):
             poeUmNaDiagonalPrincipalNaLinha(i, matriz)
-      #  for col in range(length-1):
-        #    seNaoETudoZero(matriz, 0)
-         #   print(matriz)
-        seNaoETudoZero(matriz, 0)
+
         print(matriz)
+
 
 __main__(matriz)
